2756b598ab340a6ce6332090f5d47c796175f2a2
unknown <simrankaur.june11@gmail.com>
2019-07-19 16:15:16 +0530

    first commit - test

---
+++ src/testing/TestStudent1.java
@@ -0,0 +1,17 @@
+package testing;
+
+class TestStudent1{  
+int id;  
+String name;  
+//method to display the value of id and name  
+void display(){System.out.println(id+" "+name);}  
+  
+public static void main(String args[]){  
+//creating objects  
+	TestStudent1 s1=new TestStudent1();  
+	TestStudent1 s2=new TestStudent1();  
+//displaying values of the object  
+s1.display();  
+s2.display();  
+}  
+}   
